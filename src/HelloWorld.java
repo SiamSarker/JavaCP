9c7cad9ca217aa1fb31e26b04b3db69af69999d9
siamsarker <sarkersiam2@gmail.com>
2022-09-26 01:45:27 +0600

    BeeCrowd

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! again");
+        int n;
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        for (int i=0; i<n; i++)
+        {
+            int a = sc.nextInt();
+            if (a%2==0) System.out.println("0");
+            else System.out.println("1");
+        }
     }
 }
